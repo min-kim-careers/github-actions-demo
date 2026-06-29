be78566b226fecec768a31829ca797aea640204d
min-kim-careers <min.kim.careers@gmail.com>
2026-06-29 20:30:21 +1000

    .

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM golang:alpine AS base
+
+WORKDIR /go/src/app
+
+ADD . .
+
+RUN go mod init && go build -o helloworld
+
+EXPOSE 6111
+
+CMD ["./helloworld"]
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request){
+    greeting := os.Getenv("GREETING")
+    fmt.Fprintf(w, greeting)
+}
+
+func main() {
+    http.HandleFunc("/", helloWorld)
+    http.ListenAndServe(":6112", nil)
+}
